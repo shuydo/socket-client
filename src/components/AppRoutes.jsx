fdd93cc384606144ed8b7f557fe2d70b7afbbc38
Shuydo <shuyd012@gmail.com>
2023-01-17 06:04:18 +0200

    finish server

---
+++ src/components/AppRoutes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Main from "./Main";
+import Chat from "./Chat";
+
+const AppRoutes = () => (
+  <Routes>
+    <Route path="/" element={<Main />} />
+    <Route path="/chat" element={<Chat />} />
+  </Routes>
+);
+export default AppRoutes;
---
+++ src/components/Main.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "../styles/Main.module.css";
+
+const FIELDS = {
+  NAME: "name",
+  ROOM: "room",
+};
+
+const Main = () => {
+  const { NAME, ROOM } = FIELDS;
+
+  const [values, setValues] = useState({ [NAME]: "", [ROOM]: "" });
+
+  const handleChange = ({ target: { value, name } }) =>
+    setValues({ ...values, [name]: value });
+  //запрещает переходы
+  const handleClick = e => {
+    const isDisabled = Object.values(values).some(value => !value); // console.log(isDisabled);
+    if (isDisabled) e.preventDefault();
+  };
+  // console.log(values);
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.container}>
+        <h1 className={styles.heading}>Join</h1>
+
+        <form className={styles.form}>
+          <div className={styles.group}>
+            <input
+              type="text"
+              name="name"
+              value={values[NAME]}
+              placeholder="Username"
+              className={styles.input}
+              onChange={handleChange}
+              autoComplete="off"
+              required //
+            />
+          </div>
+          <div className={styles.group}>
+            <input
+              type="text"
+              name="room"
+              value={values[ROOM]}
+              placeholder="Room"
+              className={styles.input}
+              onChange={handleChange}
+              autoComplete="off"
+              required
+            />
+          </div>
+          <Link
+            className={styles.group}
+            onClick={handleClick}
+            to={`/chat?name=${values[NAME]}&room=
+            ${values[ROOM]}`}
+          >
+            <button type="submit" className={styles.button}>
+              Sign In
+            </button>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
